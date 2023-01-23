36dedfe85d41cc79c04cf982085878bf00827854
Saher Ghanem <saherghanem333@gmail.com>
2023-01-23 07:36:24 +0200

    LED Driver - version-1 - HAL Layer

---
+++ SWC/HAL/LED/LED.c
@@ -0,0 +1,32 @@
+
+#include "LED.h"
+#include "LED_Cfg.h"
+//#include "LED_Prv.h"
+
+LED_tenuErrorStatus LED_enuLED_ON(LED_tstrLEDCfg_t Cpy_u8LED_NUMBER){
+	if(Cpy_u8LED_NUMBER.LED_PIN_NUM<0 || Cpy_u8LED_NUMBER.LED_PIN_NUM>32){
+		return LED_enuNotOK;
+	}
+	if(Cpy_u8LED_NUMBER.LED_Polarity == ACTIVE_HIGH){
+		DIO_enuSetPin(Cpy_u8LED_NUMBER.LED_PIN_NUM);
+	}else{
+
+		DIO_enuClearPin(Cpy_u8LED_NUMBER.LED_PIN_NUM);
+	}
+
+	return LED_enuOK;
+}
+
+LED_tenuErrorStatus LED_enuLED_OFF(LED_tstrLEDCfg_t Cpy_u8LED_NUMBER){
+	if(Cpy_u8LED_NUMBER.LED_PIN_NUM<0 || Cpy_u8LED_NUMBER.LED_PIN_NUM>32){
+		return LED_enuNotOK;
+	}
+	if(Cpy_u8LED_NUMBER.LED_Polarity == ACTIVE_HIGH){
+		DIO_enuClearPin(Cpy_u8LED_NUMBER.LED_PIN_NUM);
+	}else{
+
+		DIO_enuSetPin(Cpy_u8LED_NUMBER.LED_PIN_NUM);
+	}
+
+		return LED_enuOK;
+}
